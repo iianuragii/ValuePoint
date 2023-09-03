28b86eb1d33edba410590b4ac2b528ec48c37910
Anurag Dutta <imanurag18@gmail.com>
2023-09-03 22:54:44 +0530

    Create Val.sol

---
+++ Val.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract memoryPoint
+{
+    int value;      //Global variable
+
+    struct Memory{
+        int value;    
+        bytes32 name;           //Structure named Memory
+    }
+
+    Memory[] public arr;  //Array arr declared of type struct Memory
+
+    mapping (bytes32 => int)public map;    //bytes32 is same as string but bounded
+
+    function setMemory(int a)public {
+        value = a;              //value is set
+    }
+
+    function getMemory() public view returns (int){
+        return value;           //value is returned
+    }
+
+    function addMemory(bytes32 s, int a)public {
+        arr.push(Memory(a,s));
+        map[s] = a;              //Array elements are pushed in the form of dictionary
+    }
+}
